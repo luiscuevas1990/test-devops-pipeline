2d200d089e0e6621ba58f7e8ed3854e4f6168fe4
Luis Cuevas <luis.cuevas.1990@gmail.com>
2024-02-22 14:32:08 -0600

    initial commit

---
+++ src/main/java/com/example/testdevopspipeline/TestDevopsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdevopspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDevopsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDevopsPipelineApplication.class, args);
+	}
+
+}
