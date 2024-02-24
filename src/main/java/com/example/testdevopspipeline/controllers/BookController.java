671bf5abad1e83a751b51000fa6f1550e35a3227
Luis Cuevas <luis.cuevas.1990@gmail.com>
2024-02-24 12:46:28 -0600

    Adding GraphQL Controller

---
+++ src/main/java/com/example/testdevopspipeline/controllers/BookController.java
@@ -0,0 +1,23 @@
+package com.example.testdevopspipeline.controllers;
+
+import com.example.testdevopspipeline.model.Author;
+import com.example.testdevopspipeline.model.Book;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class BookController {
+
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+}
